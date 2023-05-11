c174f116684678aa8effa56e787fe0a65a21f638
Piotr Krajewski <piotrkra299@gmail.com>
2023-05-11 18:41:43 +0200

    6.2 First unit test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,45 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("Test - first unit test:");
+
+        Calculator calculator = new Calculator(20,11);
+
+        int resultA = calculator.getA();
+        int resultB = calculator.getB();
+        int resultAB = resultA + resultB;
+        int resultAdd = calculator.add();
+
+        if (resultAB == resultAdd) {
+            System.out.println("Test add calculator OK");
+        } else {
+            System.out.println("Error add calculator");
+        }
+
+        Calculator calculator1 = new Calculator(45,17);
+
+        int resultA1 = calculator1.getA();
+        int resultB1 = calculator1.getB();
+        int resultA1B1 = resultA1 - resultB1;
+        int resultSub = calculator1.subtract();
+
+        if (resultA1B1 == resultSub) {
+            System.out.println("Test subtract calculator OK");
+        } else {
+            System.out.println("Error subtract calculator");
+        }
     }
 }
