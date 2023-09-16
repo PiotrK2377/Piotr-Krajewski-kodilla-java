d444a7aacaf50c18d16b168a566c76834760a6d6
Piotr Krajewski <piotrkra299@gmail.com>
2023-09-16 23:30:58 +0200

    24.3 Aspect programming

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/OrderDto.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/OrderDto.java
@@ -11,4 +11,10 @@ public final class OrderDto {
     public List<ItemDto> getItems() {
         return items;
     }
+    @Override
+    public String toString() {
+        return "OrderDto {" +
+                " items = " + items +
+                '}';
+    }
 }
